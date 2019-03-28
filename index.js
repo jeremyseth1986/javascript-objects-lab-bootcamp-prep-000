a29b4d619408a6ee706330c42736c87da5c7bde0
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-03-28 02:10:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 recipes = object;
-object[key] = value;
+object.key = value;
 }
