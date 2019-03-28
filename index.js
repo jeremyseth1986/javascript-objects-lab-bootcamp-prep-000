0d1f527b776cddd85ebbec5022d8bea09775287f
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-03-28 02:28:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ function deleteFromObjectByKey(object, key) {
   delete clone[key]
   return clone
 }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
